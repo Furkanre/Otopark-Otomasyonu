d234a93fbf0263617bea11fb3177e7a1a8df5d7a
fruko <fruko@EXC>
2023-09-19 21:12:47 +0300

    Proje dosyası ekle.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Otopark_Otomasyonu
+{
+    /// <summary>
+    /// MainWindow.xaml etkileşim mantığı
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            button.Click+=Button_Click1;
+            btn1.Click+=Btn1_Click;
+        }
+
+        private void Btn1_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            bool renkk = true;
+            if (renkk==true)
+            {
+                btn1.Background = new SolidColorBrush(Color.FromRgb(8, 151, 46));
+                renkk=false;
+            }
+            else if (renkk==false)
+            {
+                btn1.Background = new SolidColorBrush(Colors.Red);
+            }
+        }
+    }
+}
